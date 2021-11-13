abac98bd01f26c1b75c0dad183ba42bc088e8d2d
mohamedsmile999@gmail.com <3595439Hn>
2021-11-13 06:49:34 +0200

    work on product screen just for test

--- yodawy/src/main/java/com/neqabty/yodawy/core/data/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.neqabty.yodawy.core.data
-
-import com.neqabty.yodawy.modules.orders.data.model.request.ItemRequest
-
-object Constants {
-    var itemRequests: MutableList<CartItems> = mutableListOf()
-}
--- yodawy/src/main/java/com/neqabty/yodawy/modules/products/presentation/view/productscreen/SearchActivity.kt
+++ yodawy/src/main/java/com/neqabty/yodawy/modules/products/presentation/view/productscreen/SearchActivity.kt
@@ -1,31 +1,21 @@
 package com.neqabty.yodawy.modules.products.presentation.view.productscreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.*
 import androidx.recyclerview.widget.RecyclerView
 import com.neqabty.yodawy.R
 import com.neqabty.yodawy.core.RetrofitModule
-import com.neqabty.yodawy.core.data.CartItems
-import com.neqabty.yodawy.core.data.Constants
 import com.neqabty.yodawy.modules.CartActivity
 import com.neqabty.yodawy.modules.Medication
-import com.neqabty.yodawy.modules.address.presentation.view.adressscreen.AddressViewModel
-import com.neqabty.yodawy.modules.products.data.api.ProductApi
 import com.neqabty.yodawy.modules.products.data.model.search.Data
 import com.neqabty.yodawy.modules.products.data.model.search.SearchResponse
-import com.neqabty.yodawy.modules.products.domain.entity.ProductEntity
-import com.vlonjatg.progressactivity.ProgressRelativeLayout
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
 import retrofit2.Callback
@@ -57,9 +47,6 @@ class SearchActivity : AppCompatActivity() {
 //        }
 
 
-
-
-
         viewModel = ViewModelProviders.of(this).get(PhoneViewModel::class.java)
         viewModel!!.sendToken("panadol")?.observe(this,
             Observer<ViewState<SearchResponse>> { viewState ->
@@ -67,11 +54,9 @@ class SearchActivity : AppCompatActivity() {
                     ViewState.Status.LOADING -> {
                     }
                     ViewState.Status.SUCCESS -> {
-                        Log.e("gfgh", "it[0].name")
                         mAdapter.submitList(viewState.data?.data)
                     }
                     ViewState.Status.ERROR -> {
-                        Log.e("gfgh", viewState.message.toString())
                     }
 
                 }
@@ -92,130 +77,98 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         toolbar.findViewById<ImageView>(R.id.back_btn).setOnClickListener { finish() }
-        toolbar.findViewById<FrameLayout>(R.id.cart).setOnClickListener { startActivity(Intent(this, CartActivity::class.java)) }
+        toolbar.findViewById<FrameLayout>(R.id.cart)
+            .setOnClickListener { startActivity(Intent(this, CartActivity::class.java)) }
 
-//        setDate()
     }
 
-
-    private fun setDate(){
-        var medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 2, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 2, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 3, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-        medication = Medication("name", 1, "image")
-        list.add(medication)
-    }
-}
-
-class PhoneViewModel: ViewModel() {
-
-    private var responseLiveData: LiveData<ViewState<SearchResponse>>? = null
-
-    fun sendToken(token: String): LiveData<ViewState<SearchResponse>>? {
-        responseLiveData = RemoteRepository().login(token)
-        return responseLiveData
+    class PhoneViewModel : ViewModel() {
+        private var responseLiveData: LiveData<ViewState<SearchResponse>>? = null
+        fun sendToken(token: String): LiveData<ViewState<SearchResponse>>? {
+            responseLiveData = RemoteRepository().login(token)
+            return responseLiveData
+        }
     }
 
-}
+    class RemoteRepository {
 
+        private val serviceApi: ServiceApi = RetrofitModule.instance!!.getService()
+        private val loginResponse: MutableLiveData<ViewState<SearchResponse>> =
+            MutableLiveData<ViewState<SearchResponse>>()
 
-class RemoteRepository {
-
-    private val serviceApi: ServiceApi = RetrofitModule.instance!!.getService()
-    private val loginResponse: MutableLiveData<ViewState<SearchResponse>> =
-        MutableLiveData<ViewState<SearchResponse>>()
-
-
-
-    fun login(key: String): LiveData<ViewState<SearchResponse>> {
-        serviceApi.login(key).enqueue(object : Callback<SearchResponse> {
-            override fun onResponse(call: Call<SearchResponse>?, response: Response<SearchResponse>) {
-                when {
-                    response.code() == 200 -> {// return success
-                        loginResponse.value = ViewState.success(response.body()!!)
-                    }
-                    response.code() == 401 -> {// failure
-                        loginResponse.value = ViewState.error("Wrong User id or Password")
-                    }
-                    else -> {
-                        loginResponse.value = ViewState.error("Bad Request")
+        fun login(key: String): LiveData<ViewState<SearchResponse>> {
+            serviceApi.login(key).enqueue(object : Callback<SearchResponse> {
+                override fun onResponse(
+                    call: Call<SearchResponse>?,
+                    response: Response<SearchResponse>
+                ) {
+                    when {
+                        response.code() == 200 -> {// return success
+                            loginResponse.value = ViewState.success(response.body()!!)
+                        }
+                        response.code() == 401 -> {// failure
+                            loginResponse.value = ViewState.error("Wrong User id or Password")
+                        }
+                        else -> {
+                            loginResponse.value = ViewState.error("Bad Request")
+                        }
                     }
                 }
+
+                override fun onFailure(call: Call<SearchResponse>?, t: Throwable) {
+                    loginResponse.value = ViewState.error(t.message)
+                }
+            })
+            return loginResponse
+        }
+
+    }
+
+    @JvmSuppressWildcards
+    interface ServiceApi {
+
+        @FormUrlEncoded
+        @POST("search/products")
+        fun login(@Field("search_term") keyWord: String): Call<SearchResponse>
+
+    }
+
+    class ViewState<T> private constructor(
+        val status: Status,
+        val data: T?,
+        val message: String?
+    ) {
+
+        enum class Status {
+            SUCCESS, ERROR, LOADING
+        }
+
+        companion object {
+
+            fun <T> success(data: T): ViewState<T> {
+                return ViewState(
+                    Status.SUCCESS,
+                    data,
+                    null
+                )
             }
 
-            override fun onFailure(call: Call<SearchResponse>?, t: Throwable) {
-                loginResponse.value = ViewState.error(t.message)
+            fun <T> error(msg: String?): ViewState<T> {
+                return ViewState(
+                    Status.ERROR,
+                    null,
+                    msg
+                )
             }
-        })
-        return loginResponse
-    }
 
-}
-
-@JvmSuppressWildcards
-interface ServiceApi {
-
-    @FormUrlEncoded
-    @POST("search/products")
-    fun login(@Field("search_term")keyWord: String): Call<SearchResponse>
-
-}
-class ViewState<T> private constructor(
-    val status: Status,
-    val data: T?,
-    val message: String?
-) {
-
-    enum class Status {
-        SUCCESS, ERROR, LOADING
-    }
-
-    companion object {
-
-        fun <T> success(data: T): ViewState<T> {
-            return ViewState(
-                Status.SUCCESS,
-                data,
-                null
-            )
+            fun <T> loading(): ViewState<T> {
+                return ViewState(
+                    Status.LOADING,
+                    null,
+                    null
+                )
+            }
         }
 
-        fun <T> error(msg: String?): ViewState<T> {
-            return ViewState(
-                Status.ERROR,
-                null,
-                msg
-            )
-        }
-
-        fun <T> loading(): ViewState<T> {
-            return ViewState(
-                Status.LOADING,
-                null,
-                null
-            )
-        }
     }
-
 }
