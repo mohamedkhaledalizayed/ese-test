2980cf31cd579bbb4bf5730ea0dab1f710fd090a
mohamedsmile999@gmail.com <3595439Hn>
2021-11-14 08:44:53 +0200

    view state in OrdersActivity.kt

--- yodawy/src/main/java/com/neqabty/yodawy/modules/orders/presentation/view/orderstatusscreen/OrdersActivity.kt
+++ yodawy/src/main/java/com/neqabty/yodawy/modules/orders/presentation/view/orderstatusscreen/OrdersActivity.kt
@@ -8,6 +8,7 @@ import com.neqabty.yodawy.R
 import com.neqabty.yodawy.core.data.Constants
 import com.neqabty.yodawy.databinding.ActivityOrdersBinding
 import com.neqabty.yodawy.core.ui.BaseActivity
+import com.neqabty.yodawy.core.utils.Status
 import com.neqabty.yodawy.modules.OrderDetailsActivity
 import com.vlonjatg.progressactivity.ProgressRelativeLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,12 +26,27 @@ class OrdersActivity : BaseActivity<ActivityOrdersBinding>() {
 
         setupToolbar(titleResId = R.string.orders_history)
 
-        binding.progressActivity.showLoading()
         ordersViewModel.getOrders(Constants.mobileNumber, 10, 0)
         ordersViewModel.orders.observe(this){
-            binding.progressActivity.showContent()
-            findViewById<RecyclerView>(R.id.orders_recycler).adapter = mAdapter
-            mAdapter.submitList(it)
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.LOADING -> {
+                        binding.progressActivity.showLoading()
+                    }
+                    Status.SUCCESS -> {
+                        if (resource.data!!.isEmpty()){
+                            binding.progressActivity.showEmpty(R.drawable.ic_undraw_empty_xct9, "لا يوجد طلبات", "لم تقم باى طلب من قبل")
+                        }else{
+                            binding.progressActivity.showContent()
+                            binding.ordersRecycler.adapter = mAdapter
+                            mAdapter.submitList(resource.data)
+                        }
+                    }
+                    Status.ERROR -> {
+                        binding.progressActivity.showEmpty(R.drawable.ic_undraw_access_denied_6w73, "خطا", resource.message)
+                    }
+                }
+            }
         }
         mAdapter.onItemClickListener = object :
             OrdersAdapter.OnItemClickListener {
--- yodawy/src/main/java/com/neqabty/yodawy/modules/orders/presentation/view/orderstatusscreen/OrdersViewModel.kt
+++ yodawy/src/main/java/com/neqabty/yodawy/modules/orders/presentation/view/orderstatusscreen/OrdersViewModel.kt
@@ -3,6 +3,7 @@ package com.neqabty.yodawy.modules.orders.presentation.view.orderstatusscreen
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.neqabty.yodawy.core.utils.Resource
 import com.neqabty.yodawy.modules.orders.domain.entity.OrderEntity
 import com.neqabty.yodawy.modules.orders.domain.interactors.GetOrdersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,22 +11,54 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
 class OrdersViewModel @Inject constructor(private val getOrdersUseCase: GetOrdersUseCase) :
     ViewModel() {
-    val orders = MutableLiveData<List<OrderEntity>>()
+    val orders = MutableLiveData<Resource<List<OrderEntity>>>()
     val errorMessage = MutableStateFlow("")
     fun getOrders(mobileNumber:String,pageSize:Int,pageNumber:Int) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 getOrdersUseCase.build(mobileNumber, pageSize, pageNumber).collect {
-                    orders.postValue(it)
+                    orders.postValue(Resource.success(data = it))
                 }
-            } catch (e:Throwable){
-                errorMessage.emit(e.toString())
+            } catch (exception:Throwable){
+                orders.postValue(Resource.error(data = null, message = handleError(exception)))
             }
+        }
+    }
+
+    fun clearData(){
+        orders.postValue(Resource.loading(data = null))
+    }
+
+    private fun handleError(throwable: Throwable): String {
+        return if (throwable is HttpException) {
+            when (throwable.code()) {
+                400 -> {
+                    "Wrong Username or Password"
+                }
+                401 -> {
+                    "401"
+                }
+                403 -> {
+                    "You should login"
+                }
+                404 -> {
+                    "Not Found"
+                }
+                500 -> {
+                    "Something went wrong"
+                }
+                else -> {
+                    throwable.message!!
+                }
+            }
+        } else {
+            throwable.message!!
         }
     }
 }
