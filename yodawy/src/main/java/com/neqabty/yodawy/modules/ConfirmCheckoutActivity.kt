ba8fcf350b94c6e0862b493efb7e89b428f8d61e
mohamedsmile999@gmail.com <3595439Hn>
2021-11-15 13:35:36 +0200

    cart empty view

---
+++ yodawy/src/main/java/com/neqabty/yodawy/modules/ConfirmCheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.neqabty.yodawy.modules
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neqabty.yodawy.R
+
+class ConfirmCheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_checkout)
+    }
+}
