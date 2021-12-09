fb078caa9383eaf27891a50412e06b44907dc04c
mohamedsmile999@gmail.com <3595439Hn>
2021-12-09 15:03:02 +0200

    show and hide total according to total status

--- yodawy/src/main/java/com/neqabty/yodawy/modules/CartActivity.kt
+++ yodawy/src/main/java/com/neqabty/yodawy/modules/CartActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import com.neqabty.yodawy.R
 import com.neqabty.yodawy.core.data.Constants.cartItems
 import com.neqabty.yodawy.core.data.Constants.imageList
+import com.neqabty.yodawy.core.data.Constants.total_amount
 import com.neqabty.yodawy.core.ui.BaseActivity
 import com.neqabty.yodawy.databinding.ActivityCartBinding
 import com.neqabty.yodawy.modules.address.presentation.view.adressscreen.AddressesActivity
@@ -61,7 +62,7 @@ class CartActivity : BaseActivity<ActivityCartBinding>() {
             for (item in cartItems){
                 total += (item.first.regularPrice * item.second)
             }
-            binding.checkout.text = getString(R.string.go_to_checkout) + "  ${total.roundToInt()}"
+            binding.total.text = getString(R.string.total_amount) + "  ${total.roundToInt()}"
         }
     }
 
@@ -70,9 +71,13 @@ class CartActivity : BaseActivity<ActivityCartBinding>() {
         if (cartItems.isEmpty() && imageList.isEmpty()){
             binding.clEmptyCart.visibility = View.VISIBLE
             binding.checkout.visibility = View.GONE
+            binding.total.visibility = View.GONE
         }else{
             binding.clEmptyCart.visibility = View.GONE
             binding.checkout.visibility = View.VISIBLE
+            if (total_amount){
+                binding.total.visibility = View.VISIBLE
+            }
         }
 
         /////Images recyclerView
