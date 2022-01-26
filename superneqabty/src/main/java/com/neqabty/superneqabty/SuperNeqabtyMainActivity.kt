c4a238dc39405d903021375b6a83bff0a3795021
islamlotfy <islam.m7md.lotfy@gmail.com>
2022-01-26 23:28:42 +0200

    add splash module with one launcher when run super neqabty app

---
+++ superneqabty/src/main/java/com/neqabty/superneqabty/SuperNeqabtyMainActivity.kt
@@ -0,0 +1,11 @@
+package com.neqabty.superneqabty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuperNeqabtyMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_neqabty_main)
+    }
+}
