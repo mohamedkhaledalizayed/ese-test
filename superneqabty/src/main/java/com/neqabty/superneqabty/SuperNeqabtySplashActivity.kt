722e047e89cbf744e014f41de1a715b58b8921f0
islamlotfy <islam.m7md.lotfy@gmail.com>
2022-01-27 21:14:59 +0200

    merge splash into ads

--- superneqabty/src/main/java/com/neqabty/superneqabty/SuperNeqabtySplashActivity.kt
+++ superneqabty/src/main/java/com/neqabty/superneqabty/SuperNeqabtySplashActivity.kt
@@ -3,16 +3,22 @@ package com.neqabty.superneqabty
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class SuperNeqabtySplashActivity : AppCompatActivity() {
     private val SPLASH_DISPLAY_LENGTH: Long = 3000L
-
+    private val viewModel: SuperNeqabtySplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_super_neqabty_main)
-
+        viewModel.getSyndicateAds(1)
+        viewModel.ads.observe(this){
+            Toast.makeText(applicationContext,it[0].title,Toast.LENGTH_SHORT).show()
+        }
         Handler().postDelayed(Runnable {
             val mainIntent = Intent(this@SuperNeqabtySplashActivity, SuperNeqabtySplashActivity::class.java)
             startActivity(mainIntent)
