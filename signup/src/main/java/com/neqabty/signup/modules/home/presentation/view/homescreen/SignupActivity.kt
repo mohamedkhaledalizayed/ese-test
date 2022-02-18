2ada3b441b8aa8e8acd0c019126da1f9da2c4714
mohamedsmile999@gmail.com <3595439Hn>
2022-02-18 17:02:13 +0200

    handle sign up screen

--- signup/src/main/java/com/neqabty/signup/modules/home/presentation/view/homescreen/SignupActivity.kt
+++ signup/src/main/java/com/neqabty/signup/modules/home/presentation/view/homescreen/SignupActivity.kt
@@ -2,8 +2,13 @@ package com.neqabty.signup.modules.home.presentation.view.homescreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.neqabty.signup.R
+import com.neqabty.signup.core.utils.Status
+import com.neqabty.signup.modules.home.domain.entity.SignupParams
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,5 +17,41 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+
+        signupViewModel.user.observe(this){
+
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.LOADING -> {
+                    }
+                    Status.SUCCESS -> {
+                        if (resource.data!!.nationalId.isNotEmpty()){
+
+                        }else{
+                            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    Status.ERROR -> {
+                        Log.e("ik", resource.message.toString())
+                        Toast.makeText(this, resource.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+        }
+    }
+
+    fun signUp(view: View) {
+        signupViewModel.signup(
+            SignupParams(
+                entityCode = "e0005",
+                licenceNumber = "2293",
+                membershipId = "2731",
+                mobile = "01113595435",
+                nationalId = "26910160200213",
+                password = "123456"
+            )
+        )
     }
 }
--- signup/src/main/java/com/neqabty/signup/modules/home/presentation/view/homescreen/SignupViewModel.kt
+++ signup/src/main/java/com/neqabty/signup/modules/home/presentation/view/homescreen/SignupViewModel.kt
@@ -3,6 +3,8 @@ package com.neqabty.signup.modules.home.presentation.view.homescreen
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.neqabty.signup.core.utils.AppUtils
+import com.neqabty.signup.core.utils.Resource
 import com.neqabty.signup.modules.home.domain.entity.SignupParams
 import com.neqabty.signup.modules.home.domain.entity.UserEntity
 import com.neqabty.signup.modules.home.domain.interactors.SignupUseCase
@@ -17,24 +19,16 @@ import javax.inject.Inject
 @HiltViewModel
 class SignupViewModel @Inject constructor(private val signupUseCase: SignupUseCase) :
     ViewModel() {
-    val user = MutableLiveData<UserUIModel>()
-    fun signup() {
+    val user = MutableLiveData<Resource<UserUIModel>>()
+    fun signup(data: SignupParams) {
+        user.postValue(Resource.loading(data = null))
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                signupUseCase.build(
-                    SignupParams(
-                        entityCode = "e0005",
-                        licenceNumber = "2293",
-                        membershipId = "2731",
-                        mobile = "01113595439",
-                        nationalId = "26910160200213",
-                        password = "123456"
-                    )
-                ).collect {
-                    user.postValue(it.toUserUIModel())
+                signupUseCase.build(data).collect {
+                    user.postValue(Resource.success(data = it.toUserUIModel()))
                 }
             } catch (e: Throwable) {
-
+                user.postValue(Resource.error(data = null, message = AppUtils().handleError(e)))
             }
         }
     }
