e9d0e4bb3d4f22e2e78f14a8cd2568453e8c2b61
mohamedsmile999@gmail.com <3595439Hn>
2022-02-18 14:57:38 +0200

    handle view state in syndicate screen

--- superneqabty/src/main/java/com/neqabty/superneqabty/syndicates/presentation/view/homescreen/SyndicateActivity.kt
+++ superneqabty/src/main/java/com/neqabty/superneqabty/syndicates/presentation/view/homescreen/SyndicateActivity.kt
@@ -2,10 +2,14 @@ package com.neqabty.superneqabty.syndicates.presentation.view.homescreen
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.GridView
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.neqabty.superneqabty.R
+import com.neqabty.superneqabty.core.utils.Status
 import com.neqabty.superneqabty.home.view.homescreen.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,8 +26,27 @@ class SyndicateActivity : AppCompatActivity() {
 
         syndicatesViewModel.getSyndicates()
         syndicatesViewModel.syndicates.observe(this) {
-            findViewById<GridView>(R.id.gridView).adapter = mainAdapter
-            mainAdapter.submitList(it)
+
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.LOADING -> {
+                        findViewById<ProgressBar>(R.id.progress_circular).visibility = View.VISIBLE
+                    }
+                    Status.SUCCESS -> {
+                        findViewById<ProgressBar>(R.id.progress_circular).visibility = View.GONE
+                        if (resource.data!!.isNotEmpty()){
+                            findViewById<GridView>(R.id.gridView).adapter = mainAdapter
+                            mainAdapter.submitList(resource.data)
+                        }else{
+                            Toast.makeText(this, "Empty", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    Status.ERROR -> {
+                        findViewById<ProgressBar>(R.id.progress_circular).visibility = View.GONE
+                        Toast.makeText(this, resource.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
 
         }
 
--- superneqabty/src/main/java/com/neqabty/superneqabty/syndicates/presentation/view/homescreen/SyndicatesViewModel.kt
+++ superneqabty/src/main/java/com/neqabty/superneqabty/syndicates/presentation/view/homescreen/SyndicatesViewModel.kt
@@ -1,9 +1,10 @@
 package com.neqabty.superneqabty.syndicates.presentation.view.homescreen
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.neqabty.superneqabty.core.utils.AppUtils
+import com.neqabty.superneqabty.core.utils.Resource
 import com.neqabty.superneqabty.syndicates.domain.entity.SyndicateEntity
 import com.neqabty.superneqabty.syndicates.domain.interactors.GetSyndicateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,15 +16,16 @@ import javax.inject.Inject
 @HiltViewModel
 class SyndicatesViewModel @Inject constructor(private val getSyndicateUseCase: GetSyndicateUseCase) :
     ViewModel() {
-    val syndicates = MutableLiveData<List<SyndicateEntity>>()
+    val syndicates = MutableLiveData<Resource<List<SyndicateEntity>>>()
     fun getSyndicates() {
+        syndicates.postValue(Resource.loading(data = null))
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 getSyndicateUseCase.build().collect {
-                    syndicates.postValue(it)
+                    syndicates.postValue(Resource.success(data = it))
                 }
-            }catch (e:Throwable){
-                Log.e("",e.toString())
+            }catch (exception:Throwable){
+                syndicates.postValue(Resource.error(data = null, message = AppUtils().handleError(exception)))
             }
         }
     }
