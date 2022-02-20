360757199a1a49bbd6d45c460fa06ebe15c3b214
mohamedsmile999@gmail.com <3595439Hn>
2022-02-20 08:46:43 +0200

    News Details view binding, add toolbar

--- superneqabty/src/main/java/com/neqabty/superneqabty/home/view/newsdetails/NewsDetailsActivity.kt
+++ superneqabty/src/main/java/com/neqabty/superneqabty/home/view/newsdetails/NewsDetailsActivity.kt
@@ -2,22 +2,32 @@ package com.neqabty.superneqabty.home.view.newsdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import com.neqabty.superneqabty.R
+import androidx.appcompat.widget.Toolbar
+
+import com.neqabty.superneqabty.databinding.ActivityNewsDetailsBinding
 import com.neqabty.superneqabty.home.domain.entity.NewsEntity
 import com.squareup.picasso.Picasso
 
 class NewsDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsDetailsBinding
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news_details)
+        binding = ActivityNewsDetailsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         val newsItem = intent.extras?.getParcelable<NewsEntity>("news")!!
 
-        Picasso.get().load(newsItem.image).into(findViewById<ImageView>(R.id.news_image))
-        findViewById<TextView>(R.id.news_title).text = newsItem.headline
-        findViewById<TextView>(R.id.content).text = newsItem.content
-        findViewById<TextView>(R.id.news_date).text = newsItem.createdAt
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        toolbar.title = newsItem.headline
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        Picasso.get().load(newsItem.image).into(binding.newsImage)
+        binding.newsTitle.text = newsItem.headline
+        binding.content.text = newsItem.content
+        binding.newsDate.text = newsItem.createdAt
     }
 }
