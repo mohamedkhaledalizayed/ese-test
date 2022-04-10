44377a276ff3be5348711e0a0c9ed3f8fc31aee6
monawhab <monawhab@gmail.com>
2022-04-10 21:43:12 +0200

    UI

--- app/src/main/java/com/neqabty/presentation/ui/about/AboutFragment.kt
+++ app/src/main/java/com/neqabty/presentation/ui/about/AboutFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
@@ -16,8 +18,7 @@ import com.neqabty.R
 import com.neqabty.databinding.AboutFragmentBinding
 import com.neqabty.presentation.binding.FragmentDataBindingComponent
 import com.neqabty.presentation.common.BaseFragment
-import com.neqabty.presentation.entities.SyndicateUI
-import com.neqabty.presentation.ui.news.NewsFragmentDirections
+import com.neqabty.presentation.entities.SyndicateBranchUI
 import com.neqabty.presentation.util.autoCleared
 import com.neqabty.presentation.util.call
 import com.neqabty.presentation.util.openMap
@@ -79,19 +80,25 @@ class AboutFragment : BaseFragment() {
         if(state.syndicate != null && state.branches != null){
             svContent.visibility = if (state.isLoading) View.GONE else View.VISIBLE
             binding.syndicate = state.syndicate
-            (binding.rvBranches.adapter as BranchesAdapter).submitList(state.branches)
+            renderGoverns(state.branches!!)
             initializeViews(state)
         }
     }
 
     fun initializeViews(state: AboutViewState) {
-        bMap.setOnClickListener {
-            state.syndicate?.address?.let { tvAddress.openMap(it, requireContext()) }
+        binding.layoutMain.tvTitle.text = getString(R.string.mainsyndicate_title)
+        binding.layoutMain.tvAddress.text = state.syndicate?.address
+        binding.layoutMain.tvAddress.setOnClickListener {
+            state.syndicate?.address?.let { binding.layoutMain.tvAddress.openMap(it, requireContext()) }
         }
-        clPhone.setOnClickListener {
-            state.syndicate?.phone?.let { tvPhone.call(it, requireContext()) }
+
+        binding.layoutMain.tvPhone.text = state.syndicate?.phone
+        binding.layoutMain.tvPhone.setOnClickListener {
+            state.syndicate?.phone?.let { binding.layoutMain.tvPhone.call(it, requireContext()) }
         }
-        clEmail.setOnClickListener {
+
+        binding.layoutMain.tvEmail.text = state.syndicate?.email
+        binding.layoutMain.tvEmail.setOnClickListener {
             state.syndicate?.email?.let { sendEmail(it) }
         }
     }
@@ -106,6 +113,20 @@ class AboutFragment : BaseFragment() {
         if (intent.resolveActivity(requireContext().packageManager) != null)
             startActivity(intent)
     }
+
+    fun renderGoverns(branches: List<SyndicateBranchUI>) {
+        binding.spGoverns.adapter = ArrayAdapter(requireContext(), R.layout.spinner_item, branches)
+        binding.spGoverns.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val branchesList = branches.filter { it.id == (parent.getItemAtPosition(position) as SyndicateBranchUI).id }
+                (binding.rvBranches.adapter as BranchesAdapter).submitList(branchesList)
+                (binding.rvBranches.adapter as BranchesAdapter).notifyDataSetChanged()
+            }
+        }
+        binding.spGoverns.setSelection(0)
+    }
+    
 // endregion
 
     fun navController() = findNavController()
