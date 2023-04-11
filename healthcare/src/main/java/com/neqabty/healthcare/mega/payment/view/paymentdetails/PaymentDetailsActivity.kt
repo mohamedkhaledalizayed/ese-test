43caae88a2e43c96d959dd6f9f4cc38d7aa7a322
gamaldina@hotmail.com <gamaldina@hotmail.com>
2023-04-11 12:51:19 +0200

    hide delivery methods

--- healthcare/src/main/java/com/neqabty/healthcare/mega/payment/view/paymentdetails/PaymentDetailsActivity.kt
+++ healthcare/src/main/java/com/neqabty/healthcare/mega/payment/view/paymentdetails/PaymentDetailsActivity.kt
@@ -23,6 +23,7 @@ import com.neqabty.healthcare.mega.payment.domain.entity.payment.PaymentEntity
 import com.neqabty.healthcare.mega.payment.view.PaymentViewModel
 import com.neqabty.healthcare.mega.payment.view.paymentstatus.PaymentStatusActivity
 import com.neqabty.healthcare.commen.profile.view.update.UpdateInfoActivity
+import com.neqabty.healthcare.core.data.Constants.AGRI_CODE
 import com.neqabty.healthcare.core.data.Constants.TOGAREEN_CODE
 import com.payment.paymentsdk.PaymentSdkActivity
 import com.payment.paymentsdk.PaymentSdkConfigBuilder
@@ -67,6 +68,13 @@ class PaymentDetailsActivity : BaseActivity<ActivityPaymentDetailsBinding>(),
         if (sharedPreferences.code == TOGAREEN_CODE){
             binding.tvDetails.visibility = View.GONE
             binding.cardLayout.visibility = View.GONE
+            binding.tvDeliveryMethod.visibility = View.GONE
+            binding.rgDeliveryMethods.visibility = View.GONE
+            binding.deliveryFees.visibility = View.GONE
+            binding.deliveryFeesValue.visibility = View.GONE
+        }
+
+        if (sharedPreferences.code == AGRI_CODE){
             binding.tvDeliveryMethod.visibility = View.GONE
             binding.rgDeliveryMethods.visibility = View.GONE
             binding.deliveryFees.visibility = View.GONE
