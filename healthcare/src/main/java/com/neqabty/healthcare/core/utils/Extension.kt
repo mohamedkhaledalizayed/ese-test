2fa8cdd5a894cc582405a08f7f90f6dac41c7f8d
gamaldina@hotmail.com <gamaldina@hotmail.com>
2023-07-20 20:08:56 +0300

    remove unused resources

--- healthcare/src/main/java/com/neqabty/healthcare/core/utils/Extension.kt
+++ healthcare/src/main/java/com/neqabty/healthcare/core/utils/Extension.kt
@@ -1,24 +1,10 @@
 package com.neqabty.healthcare.core.utils
 
-import android.graphics.drawable.PictureDrawable
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.caverock.androidsvg.SVG
 import java.util.regex.Pattern
 
 
 fun String.replaceText(): String{
     return this.replace("\\", "/")
-}
-
-fun ImageView.loadSVG(svg: String) {
-    val trimmedSVG = svg.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\\n", "").replace('\"', '"')
-    val svg = SVG.getFromString(trimmedSVG)
-    val drawable = PictureDrawable(svg.renderToPicture())
-
-    Glide.with(this)
-        .load(drawable)
-        .into(this)
 }
 
 fun String.isMobileValid(): Boolean {
