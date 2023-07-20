46ddc2c24b70d94e9c0994ea572a33eaec7aa32c
monawhab <monawhab@gmail.com>
2023-07-20 18:38:58 +0300

    refactors

--- healthcare/src/main/java/com/neqabty/healthcare/complains/domain/repository/ComplainRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.neqabty.healthcare.complains.domain.repository
-
-import com.neqabty.healthcare.complains.domain.entity.ComplainEntity
-import kotlinx.coroutines.flow.Flow
-
-interface ComplainRepository {
-    fun getAllComplains(): Flow<List<ComplainEntity>>
-}
